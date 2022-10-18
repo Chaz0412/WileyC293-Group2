68829f71c70ede7af8445be066cbd42e16a6c5ae
Charlie Carroll <chaz0412@googlemail.com>
2022-10-18 15:47:09 +0100

    Added Project Folder

---
+++ GroupTwoMetroSystem/src/main/java/com/grouptwo/GroupTwoMetroSystem/GroupTwoMetroSystemApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwo.GroupTwoMetroSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTwoMetroSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTwoMetroSystemApplication.class, args);
+	}
+
+}
---
+++ GroupTwoMetroSystem/src/test/java/com/grouptwo/GroupTwoMetroSystem/GroupTwoMetroSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouptwo.GroupTwoMetroSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTwoMetroSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
