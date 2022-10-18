ea26be16b32070c03d62fd5b3cbe0926318786fc
Albert Sufaj <77419354+AlbertSufaj@users.noreply.github.com>
2022-10-18 16:26:28 +0100

    Moved Springboot class into demo package

--- GroupTwoMetroSystem/src/main/java/com/grouptwo/GroupTwoMetroSystem/demo/GroupTwoMetroSystemApplication.java
+++ GroupTwoMetroSystem/src/main/java/com/grouptwo/GroupTwoMetroSystem/demo/GroupTwoMetroSystemApplication.java
@@ -1,4 +1,4 @@
-package com.grouptwo.GroupTwoMetroSystem;
+package com.grouptwo.GroupTwoMetroSystem.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
