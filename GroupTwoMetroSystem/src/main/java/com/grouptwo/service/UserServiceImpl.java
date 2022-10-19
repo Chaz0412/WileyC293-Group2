5901f4d933c825c42abcfb83c3c147c6bdb98754
Albert Sufaj <77419354+AlbertSufaj@users.noreply.github.com>
2022-10-19 16:15:14 +0100

    Added mapping for sign up page

--- GroupTwoMetroSystem/src/main/java/com/grouptwo/service/UserServiceImpl.java
+++ GroupTwoMetroSystem/src/main/java/com/grouptwo/service/UserServiceImpl.java
@@ -7,22 +7,23 @@ import org.springframework.web.client.RestTemplate;
 import com.grouptwo.entity.User;
 import com.grouptwo.persistence.UserDao;
 
-
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	@Autowired
 	private UserDao userDao;
-	
+
 	@Override
 	public User loginUser(User user) {
-		
+
 //		return userDao.getUserByUserNameAndPassword(user.getUserName(), user.getPassword());
-		return restTemplate.getForObject("http://localhost:8082/login/"+user.getUserId()+"/"+user.getPassword(), User.class);
+		return restTemplate.getForObject("http://localhost:8082/login/" + user.getUserId() + "/" + user.getPassword(),
+				User.class);
 	}
+
 	@Override
 	public User addUser(User user) {
 		return userDao.insertUser(user.getFirstName(), user.getLastName(), user.getPassword(), 100);
