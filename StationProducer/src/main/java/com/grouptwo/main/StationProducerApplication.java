1afce3be83bc1871b4d4e7a632194d3135e80f81
Albert Sufaj <77419354+AlbertSufaj@users.noreply.github.com>
2022-10-20 16:29:46 +0100

    Added StationProducer

---
+++ StationProducer/src/main/java/com/grouptwo/main/StationProducerApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationProducerApplication.class, args);
+	}
+
+}
