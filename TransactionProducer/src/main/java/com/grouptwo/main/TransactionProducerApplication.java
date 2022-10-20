664ec9c751a3435c20e20e5932bc44eeb734ed25
Charlie Carroll <chaz0412@googlemail.com>
2022-10-20 20:22:53 +0100

    added transactionProducer

---
+++ TransactionProducer/src/main/java/com/grouptwo/main/TransactionProducerApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionProducerApplication.class, args);
+	}
+
+}
